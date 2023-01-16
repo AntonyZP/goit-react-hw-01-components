7d015b2d6441758120f42dfd06c9e428e15565ed
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-01-16 20:42:59 +0200

    finish Statistics

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,15 @@
 import {Profile} from 'components/Profile/Profile'
+import {Statistics} from 'components/Statistics/Statistics'
 
 import user from '../../data/user.json';
+import data from '../../data/data.json';
+console.log(Statistics)
+console.log(data)
+
 
 export const App = () => {
     return (
+      <div>
       <div>
    <Profile
   username={user.username}
@@ -13,7 +19,9 @@ export const App = () => {
   stats={user.stats}
 />
       </div>
+      <Statistics title="Upload stats" stats={data} />
+      </div>
+
     );
   };
 
-  Profile.propTypes={}
