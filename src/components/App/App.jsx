ad78f3a1cb5917d4567feb058513f100080c26b8
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-01-17 21:03:04 +0200

    finish transaction

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,38 @@
 import {Profile} from 'components/Profile/Profile'
 import {Statistics} from 'components/Statistics/Statistics'
 import {FriendList} from 'components/FriendList/FriendList'
+import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory'
 
 
 import user from '../../data/user.json';
 import data from '../../data/data.json';
 import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 
 
 
 export const App = () => {
     return (
+     <div> 
       <div>
-      <div>
+       <div>
         <div>
         <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
+ username={user.username}
+ tag={user.tag}
+ location={user.location}
+ avatar={user.avatar}
+ stats={user.stats}
 />
-        </div>
-        <Statistics title="Upload stats" stats={data} />
+       </div>
+       <Statistics title="Upload stats" stats={data} />
 
-      </div>
-      <FriendList friends={friends} />;
-      </div>
+     </div>
+     <FriendList friends={friends} />;
+     </div>
+     <TransactionHistory items={transactions} />;
+
+     </div>
 
     );
   };
